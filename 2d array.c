a06d19aaedae45982f0b5246ea679f54f46b6b5c
Charitha2501 <charithadhaarini@gmail.com>
2024-11-27 20:33:57 -0800

    Create 2d array.c

---
+++ 2d array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+ int rows,cols;
+ printf("Enter the number of rows and colums:");
+ scanf("%d %d", &rows, &cols);
+ int a[rows][cols],sum[rows][cols];
+ printf("enter elements of matrix A:\n");
+ for(int i=0; i<rows;i++){
+     for (inyt j+=0;j<cols;j++);{
+         scanf("%d",&a[i][j]);
+     }
+}
+printf(enter the elements of matrix B:\n");
+for(int i=0;i<rows;i++){
+    for(intj=0;j<cols;j++){
+        sum[i][j]=a[i][j];
+    }
+}
+printf("Resultant Matrix:\n");
+for(int i=0;i<rows;i++){
+    for(int j=0;j<cols;j=+){
+        printf("%d",sum[i][j]);
+    }
+        printf("\n");
+  
+  ]
+return 0;
+}
+    }
+}
+ }
