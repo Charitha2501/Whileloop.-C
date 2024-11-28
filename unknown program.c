6e2c9fd864ed078f99b9d45d17837e2b42bcfe59
Charitha2501 <charithadhaarini@gmail.com>
2024-11-28 16:39:57 +0530

    Create unknown program.c

---
+++ unknown program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[50],rev[50];
+    printf("Enter a string:");
+    scanf("%s",str);
+    strcpy(rev,str);
+    strrev(rev);
+    if(strcmp(str,rev)==0) {
+        printf("The string is a palindrome.\n");
+    } else {
+    }
+return 0;
+}
