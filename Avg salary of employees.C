0a8e390d66647fbce3e4696b9e800d6efd477c3e
Charitha2501 <charithadhaarini@gmail.com>
2024-11-29 12:53:14 +0530

    Create Avg salary of employees.C

---
+++ Avg salary of employees.C
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+struct Employee {
+    char name[50];
+    float salary;
+};
+
+int main() {
+    int n, i;
+    float totalSalary = 0.0, averageSalary;
+
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+
+    struct Employee employees[n];
+
+return 0;
+}
